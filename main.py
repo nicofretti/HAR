313d0752b910893493e0ef708bbacfb649a3b232
Nicolò Fretti <34170904+nicofretti@users.noreply.github.com>
2022-06-25 17:37:26 +0200

    Apply PCA and plot the projections

--- main.py
+++ main.py
@@ -1,31 +1,44 @@
-import pandas as pd
-import numpy as np
-import plots as plts
-
 if __name__ == "__main__":
+    # %%
+    # Load data and show distribution of activities
+    #
+    import pandas as pd
+    import numpy as np
+    import plots as plts
+
     train = pd.read_csv("data/train.csv")
     activities = train["Activity"].value_counts()
     activities = {
         "index": activities.index,
         "count": activities.values
     }
-    # Draw a pie chart to see the distribution of activities
     plts.activities_distribution(activities)
     x_train = train.drop(["subject", "Activity"], axis=1)
     y_train = train.Activity
     w, h = train.shape
-    # Calculate scatter matrix of the dataset
-    # 1. Calculate mean vector
-    mean_vect = x_train.mean(axis=0)
-    # 2. Calculate covariance matrix
-    cov_mat = np.cov(x_train.T)
+    # %%
+    # [task] Calculate covariance matrix with eigenvalues and eigenvectors
+    #
+    # 1. Calculate mean vector and remove the mean from the dataset
+    mu = x_train.mean(axis=0)
+    # 2. Calculate the scatter matrix
+    cov_mat = np.cov(x_train - mu, rowvar=False)
     # 3. Calculate eigenvectors and eigenvalues
     eig_vals, eig_vecs = np.linalg.eig(cov_mat)
+    eig_vecs = eig_vecs.T
     # 4. Calculate the percentage of variance explained by each eigenvector
     tot = sum(eig_vals)
-    norm_eig_val = [(i / tot) for i in sorted(eig_vals, reverse=True)]
+    norm_eig_vals = [(i / tot) for i in sorted(eig_vals, reverse=True)]
     # 5. Plot the cumulative sum of the eigenvalues
-    plts.cumulative_eigenvalues(norm_eig_val)
-    # Adding bar coverage to the plot
-    #print(mean_vect)
+    plts.cumulative_eigenvalues(norm_eig_vals)
+    # %%
+    # [task] Apply PCA to the dataset
+    #
+    n_eigenvectors = 100
+    sorted_eig_vals = np.argsort(eig_vals)[::-1]
+    p_matrix = eig_vecs[sorted_eig_vals[:n_eigenvectors]]
+    p_data = np.matmul(x_train-mu, p_matrix.T)
+    # Plot the first three principal components
+    plts.plot_pca(p_data, y_train)
+
 
--- plots.py
+++ plots.py
@@ -1,12 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from mpl_toolkits.mplot3d import Axes3D
 
+activities_colors = sns.color_palette('pastel')[0:6]
+activities = {
+    "LAYING": activities_colors[0],
+    "SITTING": activities_colors[1],
+    "STANDING": activities_colors[2],
+    "WALKING": activities_colors[3],
+    "WALKING_DOWNSTAIRS": activities_colors[4],
+    "WALKING_UPSTAIRS": activities_colors[5]
+}
 
 def activities_distribution(activities):
     # Draw a pie chart to see the distribution of activities
-    return ;
-    plt.pie(activities["count"], colors=sns.color_palette('pastel')[0:6], labels=activities["index"], autopct='%1.1f%%')
+    plt.pie(activities["count"], colors=activities_colors, labels=activities["index"], autopct='%1.1f%%')
     plt.show()
 
 
@@ -19,5 +28,16 @@ def cumulative_eigenvalues(eigenvalues):
     plt.xlabel('Principal components')
     plt.legend(loc='best')
     plt.show()
+
+def plot_pca(p_data, y_train):
+    # Compute color for each activity
+    colors = [activities[i] for i in y_train]
+    # Draw a scatter plot to see the first three principal components
+    # Draw scatter in 3d
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.scatter(p_data.iloc[:, 0], p_data.iloc[:, 1], p_data.iloc[:, 2], c=colors)
+    plt.show()
+
 
 
